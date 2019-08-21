fb15f49d6553d2496360416735f8593dcc76f14b
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-21 18:17:15 +0700

    a

--- app/src/main/java/com/example/appquanlysinhvien/MainActivity.java
+++ app/src/main/java/com/example/appquanlysinhvien/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.appquanlysinhvien;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtTen,edtLop,edtDiachi;
+    Button btnHuy,btnThem;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapview();
+    }
+
+    private void mapview() {
+        edtTen = findViewById(R.id.edittextTen);
+        edtDiachi = findViewById(R.id.edittextDiachi);
+        edtLop = findViewById(R.id.edittextLop);
+        btnHuy = findViewById(R.id.buttonCancel);
+        btnThem = findViewById(R.id.buttonAdd);
+        recyclerView = findViewById(R.id.recyclerviewSinhvien);
     }
 }
